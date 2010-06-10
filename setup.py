d35158e6c201a8ac1ad8aa731a19c6627479c7e3
Friedrich Weber <fred@reichbier.de>
2010-06-10 12:53:19 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name = 'UDisks Contrib Libraries',
+    version = '0.1',
+    package_dir = {'cream.contrib.udisks': 'udisks'},
+    packages = ['cream.contrib.udisks'],
+    )
