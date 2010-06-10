1b1711f2167b378c3179c0a87f5e4188a59ee959
Friedrich Weber <fred@reichbier.de>
2010-06-10 16:30:01 +0200

    Use pmount.

--- udisks/__init__.py
+++ udisks/__init__.py
@@ -1,6 +1,23 @@
+import os
+from subprocess import Popen, PIPE
+import re
+
+import gobject
+import gtk
+import dbus
+
 import cream.ipc
 from cream.ipc import properties
-import dbus
+from cream.util import cached_property
+
+ACTION = 'org.freedesktop.udisks.filesystem-mount'
+COOKIE = 'cream-volume-manager-yay'
+MOUNT_POINT_RE = re.compile('^mount point to be used: (.*)$', re.MULTILINE)
+
+def mount(device):
+    proc = Popen(['pmount', '-d', device], stdout=PIPE)
+    stdout = proc.communicate()[0]
+    return MOUNT_POINT_RE.search(stdout).group(1)
 
 class Device(dbus.Interface):
     def __init__(self, path):
@@ -9,25 +26,58 @@ class Device(dbus.Interface):
             'org.freedesktop.UDisks.Device'
         )
 
-    @property
-    def fstype(self):
-        return properties.get(self, self.dbus_interface, 'IdType')
+    def get_property(self, name):
+        return properties.get(self, self.dbus_interface, name)
 
-    def mount(self, fstype, options):
-        return self.FilesystemMount(fstype, options)
+    fstype = cached_property(lambda self: self.get_property('IdType'))
+    label = cached_property(lambda self: self.get_property('IdLabel'))
+    device_file = cached_property(lambda self: self.get_property('DeviceFile'))
 
-class UDisks(dbus.Interface):
+    @cached_property
+    def description(self):
+        return self.label or self.device_file
+
+    def mount(self):
+        return mount(self.device_file)
+
+class UDisks(dbus.Interface, gobject.GObject):
+    __gsignals__ = {
+        'device-mounted': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_PYOBJECT, gobject.TYPE_STRING)),
+        'device-removed': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_PYOBJECT,)),
+    }
+
     def __init__(self):
         dbus.Interface.__init__(self,
             cream.ipc.SYSTEM_BUS.get_object('org.freedesktop.UDisks', '/org/freedesktop/UDisks'),
             'org.freedesktop.UDisks'
         )
+        gobject.GObject.__init__(self)
+        self.devices = {}
         self.connect_to_signal('DeviceAdded', self.on_device_added)
+        self.connect_to_signal('DeviceRemoved', self.on_device_removed)
+
+    # !!%!%!%!%!%!%%!
+    def do_device_mounted(self, fu, ub):
+        pass
 
     def on_device_added(self, path):
         device = Device(path)
         if device.fstype:
-            device.mount(device.fstype, [])
+            mount_path = device.mount()
+            self.devices[path] = device
+            self.emit('device-mounted', device, mount_path)
+
+    def do_device_removed(self, fu):
+        pass
+
+    def on_device_removed(self, path):
+        try:
+            device = self.devices[path]
+        except KeyError:
+            pass # TODO?
+        else:
+            self.emit('device-removed', device)
+            del self.devices[path]
 
 if __name__ == '__main__':
     from dbus.mainloop.glib import DBusGMainLoop
